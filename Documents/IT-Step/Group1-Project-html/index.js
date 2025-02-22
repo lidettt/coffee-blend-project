68616d3fc0643e91712dd01c30b8782f0fa8d465
Lidet <lidetvoeun@gmail.com>
2025-02-22 21:40:54 +0700

    First Commit But without Responsive

---
+++ Documents/IT-Step/Group1-Project-html/index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let drinksBtn = document.querySelector(".drinks-btn");
+  let dessertsBtn = document.querySelector(".desserts-btn");
+  let drinks = document.querySelector(".drinks");
+  let desserts = document.querySelector(".desserts");
+
+  // Show drinks by default and hide desserts
+  drinks.style.display = "flex";
+  desserts.style.display = "none";
+  drinksBtn.classList.add("clicked-btn");
+
+  drinksBtn.addEventListener("click", function () {
+    drinks.style.display = "flex";
+    desserts.style.display = "none";
+
+    drinksBtn.classList.add("clicked-btn");
+    dessertsBtn.classList.remove("clicked-btn");
+  });
+
+  dessertsBtn.addEventListener("click", function () {
+    desserts.style.display = "flex";
+    drinks.style.display = "none";
+    dessertsBtn.classList.add("clicked-btn");
+    drinksBtn.classList.remove("clicked-btn");
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+    let windowHeight = window.innerHeight;
+    let heading = document.getElementById("header");
+
+    if (heading) {
+      // Prevents errors if header is missing
+      if (scrollPosition > windowHeight * 0.8) {
+        heading.style.backgroundColor = "black";
+      } else {
+        heading.style.backgroundColor = "transparent";
+      }
+    }
+  });
+});
